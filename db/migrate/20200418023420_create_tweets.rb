32bef860ba2219aabe16411b1eda9ea32c6b9db9
kishiyann <to5hi5_0315@yahoo.co.jp>
2020-05-10 11:34:09 +0900

    変更

--- db/migrate/20200418023420_create_tweets.rb
+++ db/migrate/20200418023420_create_tweets.rb
@@ -1,8 +1,9 @@
 class CreateTweets < ActiveRecord::Migration[5.2]
   def change
     create_table :tweets do |t|
+      t.string :title, null: false
       t.string :text
-      t.text :image
+      t.text :image, null: false
       t.integer :user_id
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -21,14 +21,16 @@ ActiveRecord::Schema.define(version: 2020_04_18_032640) do
   end
 
   create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title", null: false
     t.string "text"
-    t.text "image"
+    t.text "image", null: false
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "nickname", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -36,7 +38,6 @@ ActiveRecord::Schema.define(version: 2020_04_18_032640) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
