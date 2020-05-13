cfb8e702a419eff326e982ca61fffc4a979744bf
kishiyann <to5hi5_0315@yahoo.co.jp>
2020-05-13 13:53:08 +0900

    微調整

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,19 +2,15 @@ class Tweet < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
   has_many :comments, dependent: :destroy
-  has_many :images, dependent: :destroy
   belongs_to_active_hash :category
-  mount_uploader :image, ImageUploader
+  mount_uploader :image, ImageUploader, dependent: :destroy
 
   validates :title, presence: true
   validates :image, presence: true
   validates :category_id, presence: true
 
-  validates :title, presence: true ,length: {minimum: 15}
-  validates :image, presence: true
-
   def self.search(search)
     return Tweet.all unless search
-    Tweet.where('text LIKE(?)', "%#{search}%")
+    Tweet.where('title LIKE(?)', "%#{search}%")
   end
 end
