b8674d07b16474197446c96ac9954879b0d7e4db
kishiyann <to5hi5_0315@yahoo.co.jp>
2020-05-14 19:55:13 +0900

    修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,11 @@ class User < ApplicationRecord
   validates :email,              presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
   validates :password,           presence: true, length: { minimum: 7 }
   validates :encrypted_password, presence: true, length: { minimum: 7 }
+
+  def self.guest
+    find_or_create_by!(nickname: 'ゲストさん',email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
+    end
+  end
 end
