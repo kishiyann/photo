955f68caf2094261dbcc11c5cdf0758c7d093099
kishiyann <to5hi5_0315@yahoo.co.jp>
2020-05-15 14:29:43 +0900

    変更

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Users::PasswordsController < Devise::PasswordsController
-  # before_action :check_guest, only: :create
+  before_action :check_guest, only: :create
   # GET /resource/password/new
   # def new
   #   super
@@ -22,11 +22,11 @@ class Users::PasswordsController < Devise::PasswordsController
   #   super
   # end
 
-  # def check_guest
-  #   if params[:user][:email].downcase == 'guest@example.com'
-  #     redirect_to root_path, alert: 'ゲストユーザーの変更・削除はできません。'
-  #   end
-  # end
+  def check_guest
+    if params[:user][:email].downcase == 'guest@example.com'
+      redirect_to root_path, alert: 'ゲストユーザーの変更・削除はできません。'
+    end
+  end
 
   # protected
 
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,7 +3,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
-  # before_action :check_guest, only: %i[update destroy]
+  before_action :check_guest, only: %i[update destroy]
 
   # GET /resource/sign_up
   # def new
@@ -39,11 +39,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # def check_guest
-  #   if resource.email == 'guest@example.com'
-  #     redirect_to root_path, alert: 'ゲストユーザーの変更・削除はできません。'
-  #   end
-  # end
+  def check_guest
+    if resource.email == 'guest@example.com'
+      redirect_to root_path, alert: 'ゲストユーザーの変更・削除はできません。'
+    end
+  end
 
   # protected
 
