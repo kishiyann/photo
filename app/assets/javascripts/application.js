50aab3d00bd50bf53595309c470191e6b9403c6c
kishiyann <to5hi5_0315@yahoo.co.jp>
2020-05-15 17:23:03 +0900

    変更

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -38,12 +38,10 @@ $(function() {
 
 $(function(){
   $fileField = $('#file')
- 
   $($fileField).on('change', $fileField, function(e) {
     file = e.target.files[0]
     reader = new FileReader(),
     $preview = $("#img_field");
- 
     reader.onload = (function(file) {
       return function(e) {
         $preview.empty();
